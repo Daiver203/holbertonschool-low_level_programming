0727d20851ce9cbbdbfe32b63dafab326a2d79ec
Daiver203 <daiver1800@gmail.com>
2022-06-15 14:19:02 -0700

    6task

---
+++ hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - name of the funtion
+ * Return: value of the funtion 'main'
+*/
+
+int main(void)
+{
+	int a = 1;
+	int b = 4;
+	int c = 8;
+
+	printf("Size of a char: %d byte(s)\n", a);
+	printf("Size of an int: %d byte(s)\n", b);
+	printf("Size of a long int: %d byte(s)\n", b);
+	printf("Size of a long long int: %d byte(s)\n", c);
+	printf("Size of a float: %d byte(s)\n", b);
+	return (0);
+}
