b41ac09c31ca284356220e909b165fb2de3951bd
Daiver203 <daiver1800@gmail.com>
2022-06-17 13:46:33 -0700

    2task

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -2,17 +2,19 @@
 
 /**
  * main - name of funtion
- * Return:value 0;
+ * Return: value of the funtion 'main'
 */
 
-int main()
+int main(void)
 {
 	char lowercase_alphabet = 97;
+	char new_line = '\n';
 
 	for (lowercase_alphabet = 97; lowercase_alphabet <= 122; lowercase_alphabet++)
 	{
-		putchar( lowercase_alphabet );
+		putchar(lowercase_alphabet);
 	}
+	putchar(new_line);
 	return (0);
 }
 
