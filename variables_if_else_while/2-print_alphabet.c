830134f7f8cf27854be47021b18de2d7d91520ae
Daiver203 <daiver1800@gmail.com>
2022-06-17 13:36:47 -0700

    2task

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -8,12 +8,10 @@
 int main()
 {
 	char lowercase_alphabet = 97;
-	char new_line = '\n';
 
 	for (lowercase_alphabet = 97; lowercase_alphabet <= 122; lowercase_alphabet++)
 	{
 		putchar(lowercase_alphabet);
-		putchar(new_line);
 	}
 	return (0);
 }
