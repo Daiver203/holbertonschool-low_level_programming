a6c31a2ece38137aa38a63b68040bea3f3419c7e
Daiver203 <daivercontrerasplata@gmail.com>
2022-06-20 12:24:07 -0500

    6task

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - name is funtion
+ * Return: value of funtion 'main'
+*/
+
+int main(void)
+{
+	int number_divisible = 48;
+
+	for (number_divisible = 48; number_divisible <= 57; number_divisible++)
+	{
+		putchar(number_divisible);
+	}
+	putchar('\n');
+	return (0);
+}
