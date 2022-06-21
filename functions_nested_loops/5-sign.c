9e535f6dab4348f7e87171cd1c27747fa81ff217
Daiver203 <daivercontrerasplata@gmail.com>
2022-06-21 18:33:35 -0500

    5task

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - name of funtion
+ * @n: value of parameter
+ * Return: value dependent on an if
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	if (n == 0)
+	{
+		return (0);
+		_putchar('0');
+	}
+	if (n < 0)
+	{
+		return (-1);
+		_putchar('-');
+	}
+}
