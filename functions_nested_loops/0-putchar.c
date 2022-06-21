6ba1ff1fbc4bc37aece7ab5cee2518fee88017b7
Daiver203 <daivercontrerasplata@gmail.com>
2022-06-21 14:48:32 -0500

    0task

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - name of the funtion 'main'
- * Return: value of cero
+ * main - name of funtion
+ *
+ * Return: value to zero
 */
 
 int main(void)
 {
-	printf("_putchar");
+	char caracters[8] = "_putchar";
+	int i = 0;
+	char new_line = '\n';
+
+	while (caracters[i] != '\0')
+	{
+		putchar(caracters[i++]);
+	}
+	putchar(new_line);
 	return (0);
 }
