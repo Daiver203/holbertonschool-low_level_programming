c1ece73f1c430065e3d63240e2cbeb4a07a512e4
Daiver203 <daivercontrerasplata@gmail.com>
2022-06-22 11:35:22 -0500

    11task

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_to_98 - name of funtion
+ * @n: value of parameter
+ * Return: always zero
+*/
+
+void print_to_98(int n)
+{
+	n = 0;
+
+	while (n <= 98)
+	{
+		printf("%d, ", n);
+		n++;
+	}
+	printf("\n");
+}
