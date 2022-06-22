81338eb12293bdd05e524cda4da2e227fc7293bf
Daiver203 <daivercontrerasplata@gmail.com>
2022-06-22 10:58:31 -0500

    10task

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - name of funtion
+ * Return: result of suma
+ * @a: value one of parameters
+ * @b: value two of parameters
+*/
+
+int add(int a, int b)
+{
+	int suma;
+
+	suma = a + b;
+	return (suma);
+}
