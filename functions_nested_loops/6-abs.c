7f8df26ee0a322b3fea9cbde8c85d1d07afa3030
Daiver203 <daivercontrerasplata@gmail.com>
2022-06-21 19:11:15 -0500

    6task

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _abs - name of funtion
+ * @x: value of parameter in funtion
+ * Return: value of funtion 'short if else'
+ */
+
+int _abs(int x)
+{
+	return ((x < 0) ? -x : x);
+}
