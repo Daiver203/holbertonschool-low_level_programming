7bd418368898d49d04a252bafca1a48338384585
Daiver203 <daivercontrerasplata@gmail.com>
2022-06-28 12:15:15 -0500

    2task

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - name of funtion
+ * @a: value of parameter
+ * @b: value of parameter
+ * Return: value 0
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+
+	return (mul);
+}
