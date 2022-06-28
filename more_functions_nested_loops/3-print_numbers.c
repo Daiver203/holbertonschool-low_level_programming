e0ced6b8e31b99d2af79c90e97b92c0f8fdcafc2
Daiver203 <daivercontrerasplata@gmail.com>
2022-06-28 12:24:46 -0500

    3task

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - name of the funtion
+ * Return: value 0
+ */
+
+void print_numbers(void)
+{
+	int count = 48;
+	char new_line = 10;
+
+	while (count <= 57)
+	{
+		_putchar(count);
+		count++;
+	}
+	_putchar(new_line);
+	return (0);
+}
