015744aef87e4a9d857bce50ff3b374e92e8e223
Daiver203 <daivercontrerasplata@gmail.com>
2022-06-29 15:44:49 -0500

    5task

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - name of the funtion
+ * Return: value 0
+ */
+
+void more_numbers(void)
+{
+	char numbers;
+	int count = 0;
+	char new_line = 10;
+
+	while (count < 10)
+	{
+		for (numbers = 0; numbers <= 14; numbers++)
+		{
+			if (numbers >= 10)
+			{
+				putchar((numbers / 10) + '0');
+			}
+			putchar((numbers % 10) + '0');
+		}
+		count++;
+		putchar(new_line);
+	}
+	return (0);
+}
