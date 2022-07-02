747ef72f684e43359beec1f30e256966382872ab
Daiver203 <daivercontrerasplata@gmail.com>
2022-07-02 13:10:20 -0500

    0task

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - name of funtion
+ * @n: value of parameter
+ * Return: value always 0
+*/
+
+void reset_to_98(int *n)
+{
+	printf("n:%d\n", n);
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+#pointers_is_funny_
