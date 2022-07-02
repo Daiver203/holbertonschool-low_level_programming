cbfd1642541c594c8b44040ff6e7cdf38a168170
Daiver203 <daivercontrerasplata@gmail.com>
2022-07-02 16:37:36 -0500

    2task

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - name of funtion
+ * @s: value of parameter
+ * Return: value of the 'int'
+ */
+
+int _strlen(char *s)
+{
+	int a = 0;
+
+	while (*s[a] != '\0')
+	{
+		a++;
+	}
+	return (a);
+}
