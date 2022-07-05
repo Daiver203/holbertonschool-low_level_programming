f043b3cdae819d47a7a91d36a45d299ac8401a42
Daiver203 <daivercontrerasplata@gmail.com>
2022-07-05 00:40:11 -0500

    6task

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -8,17 +8,17 @@
 
 int is_prime_number(int n)
 {
-	return (prim(2, n));
+	return (_prim(2, n));
 }
 
 /**
- * prim - name of the function
+ * _prim - name of the function
  * @a: value to evaluate
  * @n: value know if a prime number
  * Return: n if is prime
 */
 
-int prim(int a, int n)
+int _prim(int a, int n)
 {
 	if (n <= 1 || (n != a && n % a == 0))
 		return (0);
@@ -26,5 +26,5 @@ int prim(int a, int n)
 	if (n == a)
 		return (1);
 
-	return (prim(a + 1, n));
+	return (_prim(a + 1, n));
 }
