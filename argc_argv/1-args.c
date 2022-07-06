4e3fcebb6acb32b0f1f914a227a195568b5d6877
Daiver203 <daivercontrerasplata@gmail.com>
2022-07-06 14:26:18 -0500

    1task

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - name of funtion
+ * @argc: value of parameters
+ * @argv: value of parameters
+ * Return: always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	for (int i = 0; i < argv[i]; i++)
+		printf("%d\n", i);
+
+	return (0);
+}
