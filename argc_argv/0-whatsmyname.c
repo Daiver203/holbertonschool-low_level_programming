77bd23a89ed1465290466e3d3020136137192337
Daiver203 <daivercontrerasplata@gmail.com>
2022-07-06 13:45:07 -0500

    0task

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - name if funtion
+ * @argc: value of parameters
+ * @argv: value of parameters
+ * Return:value always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	void argc;
+
+	printf("%s",argv[0]);
+	return 0;
+}
