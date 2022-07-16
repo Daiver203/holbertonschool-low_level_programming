b6c9e2b6b99fa869fb9d10eb85a129fe26e19816
Daiver203 <daivercontrerasplata@gmail.com>
2022-07-16 17:23:07 -0500

    2task

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -9,9 +9,9 @@
 
 void print_dog(struct dog *d)
 {
-	printf("Name: %s", d->name);
-	printf("Age: %f", d->age);
-	printf("Owner: %s", d->owner);
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
 
 	if (d == NULL)
 		printf("(nil)");
