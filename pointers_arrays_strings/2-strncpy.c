a3d3cc519ee60ef24b8c5375845788e7c78b91d3
Daiver203 <daivercontrerasplata@gmail.com>
2022-07-26 17:04:31 -0500

    task

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -10,15 +10,13 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int a = 0;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-	{
-		dest[i] = src[i];
-	}
-	for (i = 0; i < n; i++)
-	{
-		dest[i] = '\0';
-	}
+	for (a = 0; a < n && src[a] != '\0'; a++)
+		dest[a] = src[a];
+
+	for (; a < n; a++)
+		dest[a] = '\0';
+
 	return (dest);
 }
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 #include <stdio.h>
 void reset_to_98(int *n);
@@ -16,5 +16,13 @@ char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+int _putchar(char c);
 
-#endif
+#endif /*_MAIN_H_*/
