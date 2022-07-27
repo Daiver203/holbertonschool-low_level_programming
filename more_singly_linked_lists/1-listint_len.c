11e5a780afc0bd34cbd4fb801f3d4cd28f59649b
Daiver203 <daivercontrerasplata@gmail.com>
2022-07-27 11:09:13 -0500

    1task

---
+++ more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - return the number of nodes in a linked list
+ * @h: pointer head
+ *
+ * Return: number of nodes in the linked list
+*/
+
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		return (i);
+		i++;
+	}
+}
